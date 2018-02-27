17c1acad45db33f37065fafe341c438bcf66c7cc
Nolan Byrnes <nolanehs@gmail.com>
2018-02-27 11:51:04 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,17 @@
-class Post < ActiveRecord::Base
-  validates :content, length:{minimum: 250}
-  validates :summary, length:{maximum: 250}
-  validates :title, presence:true
-  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
-
-end
-
 class TitleValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value.match? /(Secret|Guess|Won\'t Believe|Top)/
       record.errors[attribute] << (options[:message] || "is not clickbaity title")
     end
   end
+end
+
+
+
+class Post < ActiveRecord::Base
+  validates :content, length:{minimum: 250}
+  validates :summary, length:{maximum: 250}
+  validates :title, presence:true
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
 end
