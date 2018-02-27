1105a8c303db8c83fb0f0f971d6e487b0db27156
Nolan Byrnes <nolanehs@gmail.com>
2018-02-27 11:43:41 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
   validates :content, length:{minimum: 250}
   validates :summary, length:{maximum: 250}
   validates :title, presence:true
+  validates :category, inclusion: {in: (Fiction Non-Fiction)}
 end
+
+validates :size, inclusion: { in: %w(small medium large)
